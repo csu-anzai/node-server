c7170f5bbeb5de51f5bc9e8ba73b9d7458ced511
chynamyerz <myezasifiso11@gmail.com>
2019-09-06 04:26:03 -0700

    asbone

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const server = express()
-server.get('/', (req, res) => res.send('Hello World!'))
 
 if (process.env.NODE_ENV === "test") {
-    server.get('/', (req, res) => res.send('Hello World!'))
+    server.get('/', (req, res) => res.send('Hello world!'))
 }else if(process.env.NODE_ENV === "dev") {
-    server.get('/', (req, res) => res.send('Hello World!'))
+    server.get('/', (req, res) => res.send('Hello world!'))
     server.listen({
         port: 3000
     }, () => console.log(`Example server listening on port ${port}!`))
 }else if(process.env.NODE_ENV === "prod") {
-    server.get('/', (req, res) => res.send('Hello World!'))
+    server.get('/', (req, res) => res.send('Hello world!'))
     server.listen({
         port: 4000
     })
