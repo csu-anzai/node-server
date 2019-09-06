c376a32d499ae8233e890e54488c638aaf203125
Sifiso Myeza <myezasifiso11@gmail.com>
2019-09-06 09:54:58 +0200

    opened port 8080

--- src/__tests__/server.test.js
+++ src/__tests__/server.test.js
@@ -2,17 +2,17 @@ const request = require("supertest")
 const server = require("../index")
 
 describe("GET /", () => {
- it("respond with hello world", async () => { 
+  it("respond with hello world", async () => { 
     let responce = null
     try{
       //navigate to root and check the the response is "hello world"
       responce = await request(server).get("/");
 
     } catch (e) {
-       console.log(e.message)
+        console.log(e.message)
     }
 
-   expect(responce.status).toBe(200);
-   expect(responce.text).toContain("Hello World!");
- });
+    expect(responce.status).toBe(200);
+    expect(responce.text).toContain("Hello World!");
+  });
 });
