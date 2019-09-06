fac97ce5756848c88804dcbee7a2254208a22816
chynamyerz <myezasifiso11@gmail.com>
2019-09-06 10:06:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const server = express()
 server.get('/', (req, res) => res.send('Hello World!'))
 
-if (process.env.NODE_ENV === "test"){
+if (process.env.NODE_ENV === "test") {
     server.get('/', (req, res) => res.send('Hello World!'))
-}else if(process.env.NODE_ENV === "dev"){
+}else if(process.env.NODE_ENV === "dev") {
     server.get('/', (req, res) => res.send('Hello World!'))
     server.listen({
         port: 4001
     }, () => console.log(`Example app listening on port ${port}!`))
-}else if(process.env.NODE_ENV === "prod"){
+}else if(process.env.NODE_ENV === "prod") {
     server.get('/', (req, res) => res.send('Hello World!'))
     server.listen({
         port: 4000
