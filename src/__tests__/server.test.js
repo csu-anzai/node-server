949c78a2f101860be5bf7b33a199bbd2a8db9c3f
Sifiso Myeza <myezasifiso11@gmail.com>
2019-09-06 10:04:35 +0200

    webhook

--- src/__tests__/server.test.js
+++ src/__tests__/server.test.js
@@ -4,6 +4,7 @@ const server = require("../index")
 describe("GET /", () => {
   it("respond with hello world", async () => { 
     let responce = null
+    
     try{
       //navigate to root and check the the response is "hello world"
       responce = await request(server).get("/");
